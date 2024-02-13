a4edbe085a118fdba6c2e74864a17607fb79ec6b
Alexande Hamburg <uplink205@gmail.com>
2024-02-13 13:43:17 +0200

    update gradle.yml, added checkstyle.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,25 @@
 plugins {
-    id("java")
+    application
+    checkstyle
     jacoco
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass = "hexlet.code.App"
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.16.1")
 }
 
 tasks.test {
@@ -22,5 +29,5 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+    reports { xml.required.set(true) }
 }
-
