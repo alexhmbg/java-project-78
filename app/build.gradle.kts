e7e7eecf851dda71924277eb7c33e32531ab0ae0
Alexande Hamburg <uplink205@gmail.com>
2024-02-13 13:59:05 +0200

    add Makefile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,10 +16,14 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.16.1")
+    implementation ("info.picocli:picocli:4.6.1")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
+
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation ("org.junit.jupiter:junit-jupiter-params:5.8.1")
+    testImplementation ("org.assertj:assertj-core:3.21.0")
+    testImplementation ("org.skyscreamer:jsonassert:1.5.0")
 }
 
 tasks.test {
