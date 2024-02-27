7b0df23a1255faf20d8746e8f2d0a8bf778859e1
alexhmbg <uplink205@gmail.com>
2024-02-27 13:37:57 +0200

    update tests

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 import hexlet.code.schemas.BaseSchema;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ValidatorTest {
     @Test
@@ -15,40 +15,21 @@ class ValidatorTest {
         var validator = new Validator();
         var schema = validator.string();
 
-        var expected = true;
-        var actual = schema.isValid("");
-        assertEquals(expected, actual);
 
-        var expected2 = true;
-        var actual2 = schema.isValid(null);
-        assertEquals(expected2, actual2);
+        assertTrue(schema.isValid(""));
+        assertTrue(schema.isValid(null));
 
         schema.required();
-        var expected3 = false;
-        var actual3 = schema.isValid("");
-        assertEquals(expected3, actual3);
-
-        var expected4 = false;
-        var actual4 = schema.isValid(null);
-        assertEquals(expected4, actual4);
+        assertFalse(schema.isValid(""));
+        assertFalse(schema.isValid(null));
 
         schema.contains("what");
-        var expected5 = true;
-        var actual5 = schema.isValid("what");
-        assertEquals(expected5, actual5);
-
-        var expected6 = true;
-        var actual6 = schema.isValid("whatever");
-        assertEquals(expected6, actual6);
+        assertTrue(schema.isValid("what"));
+        assertTrue(schema.isValid("whatever"));
 
         schema.minLength(5);
-        var expected7 = true;
-        var actual7 = schema.isValid("what ");
-        assertEquals(expected7, actual7);
-
-        var expected8 = false;
-        var actual8 = schema.isValid("what");
-        assertEquals(expected8, actual8);
+        assertTrue(schema.isValid("what "));
+        assertFalse(schema.isValid("what"));
     }
 
     @Test
@@ -56,36 +37,19 @@ class ValidatorTest {
         var validator = new Validator();
         var schema = validator.number();
 
-        var expected = true;
-        var actual = schema.isValid(5);
-        assertEquals(expected, actual);
-
-        var expected2 = true;
-        var actual2 = schema.isValid(null);
-        assertEquals(expected2, actual2);
+        assertTrue(schema.isValid(5));
+        assertTrue(schema.isValid(null));
 
         schema.required();
-        var expected3 = false;
-        var actual3 = schema.isValid(null);
-        assertEquals(expected3, actual3);
+        assertFalse(schema.isValid(null));
 
         schema.positive();
-        var expected4 = true;
-        var actual4 = schema.isValid(10);
-        assertEquals(expected4, actual4);
-
-        var expected5 = false;
-        var actual5 = schema.isValid(-10);
-        assertEquals(expected5, actual5);
+        assertFalse(schema.isValid(-10));
+        assertFalse(schema.isValid(0));
 
         schema.range(5, 10);
-        var expected6 = false;
-        var actual6 = schema.isValid(4);
-        assertEquals(expected6, actual6);
-
-        var expected7 = true;
-        var actual7 = schema.isValid(10);
-        assertEquals(expected7, actual7);
+        assertFalse(schema.isValid(4));
+        assertTrue(schema.isValid(10));
     }
 
     @Test
@@ -97,32 +61,16 @@ class ValidatorTest {
         var map2 = new HashMap<String, String>();
         map2.put("key1", "value1");
         map2.put("key2", "value2");
-
-        var expected = true;
-        var actual = schema.isValid(null);
-        assertEquals(expected, actual);
-
-        var expected2 = true;
-        var actual2 = schema.isValid(new HashMap<>());
-        assertEquals(expected2, actual2);
-
-        var expected3 = true;
-        var actual3 = schema.isValid(map);
-        assertEquals(expected3, actual3);
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid(new HashMap<>()));
+        assertTrue(schema.isValid(map));
 
         schema.required();
-        var expected4 = false;
-        var actual4 = schema.isValid(null);
-        assertEquals(expected4, actual4);
+        assertFalse(schema.isValid(null));
 
         schema.sizeof(2);
-        var expected5 = false;
-        var actual5 = schema.isValid(map);
-        assertEquals(expected5, actual5);
-
-        var expected6 = true;
-        var actual6 = schema.isValid(map2);
-        assertEquals(expected6, actual6);
+        assertFalse(schema.isValid(map));
+        assertTrue(schema.isValid(map2));
     }
 
     @Test
@@ -137,23 +85,17 @@ class ValidatorTest {
         Map<String, String> human1 = new HashMap<>();
         human1.put("firstName", null);
         human1.put("lastName", "Smith");
-        var expected = false;
-        var actual = schema.isValid(human1);
-        assertEquals(expected, actual);
+        assertFalse(schema.isValid(human1));
 
         Map<String, String> human2 = new HashMap<>();
         human2.put("firstName", "John");
         human2.put("lastName", "S");
-        var expected2 = false;
-        var actual2 = schema.isValid(human2);
-        assertEquals(expected2, actual2);
+        assertFalse(schema.isValid(human2));
 
         Map<String, String> human3 = new HashMap<>();
         human3.put("firstName", "Brad");
         human3.put("lastName", "Pitt");
-        var expected3 = true;
-        var actual3 = schema.isValid(human3);
-        assertEquals(expected3, actual3);
+        assertTrue(schema.isValid(human3));
     }
 }
 
